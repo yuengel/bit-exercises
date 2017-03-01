af2d147ffb68e1e1296fca8bb038d9a3e4e91f20
yuengel <oscaryuengel@gmail.com>
2017-02-28 19:51:02 -0600

    Add functional parsum.cpp with input flaws
    This program works mostly as intended, however it would benefit from
    minor refactoring and lacks input checking of any kind. Invalid input
    will produce unintended results.

---
+++ parsum.cpp
@@ -0,0 +1,119 @@
+/*
+Oscar Yuengel
+Given a string representing an algebraic sum of integers, parses it and finds its value.
+*/
+
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <sstream> // stoi() fix for MinGW
+using namespace std;
+
+int main()
+{
+	cout << "This program determines the sum of an algebraic expression of integers.\n\n"
+		 << "Enter a string representing such an expression.\n"
+		 << "The program assumes a valid expression consisting only of (0,9), +, -, and SPACE.\n"
+		 << "Please use care.\n";
+
+	string str, tmpString = "";
+	int sum = 0, tmp = 0;
+	bool add = true;
+
+	getline(cin, str, '\n');
+
+	for (string::iterator it = str.begin(), itEnd = str.end(); it != itEnd; it++)
+	{
+		switch (*it)
+		{
+			case '+':
+			{
+				if (!tmpString.empty())
+				{
+					// stoi() workaround
+					stringstream ss;
+					ss.str(tmpString);
+					ss >> tmp;
+					ss.clear();
+
+					tmpString = "";
+
+					if (add)
+						sum += tmp;
+					else
+						sum -= tmp;
+				}
+
+				add = true;
+
+			} break;
+
+			case '-':
+			{
+
+				if (!tmpString.empty())
+				{
+					// stoi() workaround
+					stringstream ss;
+					ss.str(tmpString);
+					ss >> tmp;
+					ss.clear();
+
+					tmpString = "";
+
+					if (add)
+						sum += tmp;
+					else
+						sum -= tmp;
+				}
+
+				add = false;
+
+			} break;
+
+			case ' ':
+			{
+				if (!tmpString.empty())
+				{
+					// stoi() workaround
+					stringstream ss;
+					ss.str(tmpString);
+					ss >> tmp;
+					ss.clear();
+
+					tmpString = "";
+
+					if (add)
+						sum += tmp;
+					else
+						sum -= tmp;
+				}
+
+
+			} break;
+
+			// In case of numeral
+			default:
+			{
+				tmpString += *it;
+			} break;
+		}
+	}
+
+	if (!tmpString.empty())
+	{
+		// stoi() workaround
+		stringstream ss;
+		ss.str(tmpString);
+		ss >> tmp;
+		ss.clear();
+
+		tmpString = "";
+
+		if (add)
+			sum += tmp;
+		else
+			sum -= tmp;
+	}
+	cout << sum;
+}
