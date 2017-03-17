7ca9ef4638c955b6363b0966146a7df0412bdbc7
yuengel <oscaryuengel@gmail.com>
2017-03-16 23:56:34 -0500

    Tidy up helpers files

--- helpers.cpp
+++ helpers.cpp
@@ -1,83 +1,80 @@
-/*
-Oscar Yuengel
-Implementation file for helper functions.
-*/
+/**************
+* Oscar Yuengel
+*
+* Implementation file for helper functions.
+******************************************/
 
-#include "helpers.h" // <vector>, <string>
+#include "helpers.h"
 
 namespace my {
 
-	std::vector<std::string> tokenize(const std::string str, const std::string delims)
-	{
-		std::vector<std::string> v;
-		std::string tmp;
-		int prev = 0;
-		size_t location = str.find_first_of(delims, prev);
+std::vector<std::string> tokenize(const std::string str, const std::string delims) {
+	std::vector<std::string> v;
+	std::string tmp;
+	int prev = 0;
+	size_t location = str.find_first_of(delims, prev);
+	
+	while (location != std::string::npos) {
+		tmp = str.substr(prev, location - prev);
 		
-		while (location != std::string::npos)
-		{
-			tmp = str.substr(prev, location - prev);
-			// ensure token exists
-			if (!tmp.empty())
-				v.push_back(tmp);
-
-			prev = location + 1;
-			location = str.find_first_of(delims, prev);
-		}
-		// last token
-		tmp = str.substr(prev);
-
+		// Ensure token exists
 		if (!tmp.empty())
 			v.push_back(tmp);
 
-		return v;
+		prev = location + 1;
+		location = str.find_first_of(delims, prev);
 	}
+	
+	tmp = str.substr(prev);
 
-	int stoi(const std::string str)
-	{
-		// Invalid characters
-		if (str.find_first_not_of("-1234567890 ") != std::string::npos)
-			throw std::invalid_argument("str must contain a valid integer.\n");
+	if (!tmp.empty())
+		v.push_back(tmp);
 
-		std::stringstream ss;
-		long long int llNum;
-		
-		ss.str(str);
-		ss >> llNum >> std::ws; // captures trailing whitespace
-		// More than one integer entered
-		if (!ss.eof())
-			throw std::invalid_argument("str must contain a single valid integer.\n");
-
-		if (llNum > std::numeric_limits<int>::max() ||
-			llNum < std::numeric_limits<int>::min())
-			throw std::out_of_range("str must contain an integer within int range.\n");
-
-		int num = llNum;
-
-		return num;
-	}
-
-	double stod(const std::string str)
-	{
-		// Invalid characters
-		if (str.find_first_not_of("-1234567890. ") != std::string::npos)
-			throw std::invalid_argument("str must contain a valid double.\n");
-
-		std::stringstream ss;
-		long double lNum;
-		
-		ss.str(str);
-		ss >> lNum >> std::ws; // captures trailing whitespace
-		// More than one integer entered
-		if (!ss.eof())
-			throw std::invalid_argument("str must contain a single valid double.\n");
-
-		if (lNum > std::numeric_limits<double>::max() ||
-			lNum < std::numeric_limits<double>::min())
-			throw std::out_of_range("str must contain an integer within double range.\n");
-
-		double num = lNum;
-
-		return num;
-	}
+	return v;
 }
+
+int stoi(const std::string str) {
+	// Invalid characters
+	if (str.find_first_not_of("-1234567890 ") != std::string::npos)
+		throw std::invalid_argument("str must contain a valid integer.\n");
+
+	std::stringstream ss;
+	long long int llNum;
+	ss.str(str);
+	ss >> llNum >> std::ws;  // captures trailing whitespace
+	
+	if (!ss.eof())
+		throw std::invalid_argument("str must contain a single valid integer.\n");
+
+	if (llNum > std::numeric_limits<int>::max() ||
+		llNum < std::numeric_limits<int>::min())
+		throw std::out_of_range("str must contain an integer within int range.\n");
+
+	int num = llNum;
+
+	return num;
+}
+
+double stod(const std::string str) {
+	// Invalid characters
+	if (str.find_first_not_of("-1234567890. ") != std::string::npos)
+		throw std::invalid_argument("str must contain a valid double.\n");
+
+	std::stringstream ss;
+	long double lNum;
+	ss.str(str);
+	ss >> lNum >> std::ws;  // captures trailing whitespace
+	
+	if (!ss.eof())
+		throw std::invalid_argument("str must contain a single valid double.\n");
+
+	if (lNum > std::numeric_limits<double>::max() ||
+		lNum < std::numeric_limits<double>::min())
+		throw std::out_of_range("str must contain an integer within double range.\n");
+
+	double num = lNum;
+
+	return num;
+}
+
+}  // namespace my
--- helpers.h
+++ helpers.h
@@ -1,7 +1,8 @@
-/*
-Oscar Yuengel
-Header file for helper functions.
-*/
+/**************
+* Oscar Yuengel
+*
+* Header file for helper functions.
+**********************************/
 
 #ifndef HELPERS_H
 #define HELPERS_H
@@ -13,12 +14,14 @@ Header file for helper functions.
 #include <stdexcept>
 
 namespace my {
+
 	// Tokenizes a string around given delimiters. Returns a vector containing the tokens. 
 	std::vector<std::string> tokenize(const std::string str, const std::string delims);
 	// Converts a string into its corresponding integer. Throws exceptions on invalid inputs.
 	int stoi(const std::string str);
 	// Converts a string into its corresponding double. Throws exceptions on invalid inputs.
 	double stod(const std::string str);
-}
+
+}  // namespace my
 
 #endif
